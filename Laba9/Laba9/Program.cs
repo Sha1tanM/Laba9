296fa9e40f0cc24fe55453a1be5ed78e49651086
Igor <97942261+Sha1tanM@users.noreply.github.com>
2023-05-09 22:01:53 +0300

    Add files via upload

---
+++ Laba9/Laba9/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.Write("Введите латинский символ: ");
+        char latinChar = Console.ReadKey().KeyChar;
+        Console.WriteLine();
+
+        if (latinChar >= 'A' && latinChar <= 'Z')
+        {
+            char cyrillicChar = (char)(latinChar - 'A' + 'А');
+            Console.WriteLine($"Похожий кириллический символ: {cyrillicChar} ({(int)latinChar}) [{(int)cyrillicChar}]");
+        }
+        else if (latinChar >= 'a' && latinChar <= 'z')
+        {
+            char cyrillicChar = (char)(latinChar - 'a' + 'а');
+            Console.WriteLine($"Похожий кириллический символ: {cyrillicChar} ({(int)latinChar}) [{(int)cyrillicChar}]");
+        }
+        else
+        {
+            Console.WriteLine("Введенный символ не является латинским.");
+        }
+
+        Console.ReadKey();
+    }
+}
